35d83c1c5b17ecf3692c6e4d7d64f5422f956e2b
Olgert Denas <denas@adobe.com>
2016-10-14 01:06:51 -0700

    Initial Commit

---
+++ bit-vector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bit-vector
+//
+//  Created by denas on 10/14/16.
+//  Copyright © 2016 denas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ fd_ms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  fd_ms
+//
+//  Created by denas on 10/14/16.
+//  Copyright © 2016 denas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
